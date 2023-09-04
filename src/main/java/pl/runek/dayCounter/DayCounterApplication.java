f859a25bf934ad6ad5bc0061a79f0e1625235e70
Runek00 <runek00@gmail.com>
2023-09-04 22:04:46 +0200

    Let's begin!

---
+++ src/main/java/pl/runek/dayCounter/DayCounterApplication.java
@@ -0,0 +1,13 @@
+package pl.runek.dayCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/runek/dayCounter/DayCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.runek.dayCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
